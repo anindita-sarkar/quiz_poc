680565e40b929948bca7c1d1e461f90b63231c30
Abhinav Anand <cyberaka@gmail.com>
2018-01-07 15:54:24 +0530

    Modification done to create deployable war

--- codebase/quiz-parent/quiz-ws/src/main/java/com/cyberaka/quiz/QuizBootApplication.java
+++ codebase/quiz-parent/quiz-ws/src/main/java/com/cyberaka/quiz/QuizBootApplication.java
@@ -1,12 +1,14 @@
 package com.cyberaka.quiz;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.logging.Logger;
 
 /**
  * Entry point into the quiz boot application.
@@ -16,20 +18,26 @@ import java.util.logging.Logger;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.cyberaka.quiz.dao")
 @EntityScan(basePackages = "com.cyberaka.quiz.domain")
-@ComponentScan(basePackages = {"com.cyberaka.quiz", "com.cyberaka.quiz.rest", "com.cyberaka.quiz.dao", "com.cyberaka.quiz.service.impl"})
-public class QuizBootApplication {
+@ComponentScan(basePackages = { "com.cyberaka.quiz", "com.cyberaka.quiz.rest", "com.cyberaka.quiz.dao",
+		"com.cyberaka.quiz.service.impl" })
+public class QuizBootApplication extends SpringBootServletInitializer {
 
-    static final Logger LOG = Logger.getLogger(QuizBootApplication.class.getName());
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(QuizBootApplication.class);
+	}
 
-    public QuizBootApplication() {
-        // Default constructor.
-    }
+	static final Logger LOG = Logger.getLogger(QuizBootApplication.class.getName());
 
-    public static void main(String[] args) {
-        SpringApplication.run(QuizBootApplication.class, args);
-    }
+	public QuizBootApplication() {
+		// Default constructor.
+	}
 
-    public void info() {
-        LOG.info("Quiz Boot Applicaiton");
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(QuizBootApplication.class, args);
+	}
+
+	public void info() {
+		LOG.info("Quiz Boot Applicaiton");
+	}
 }

d9aa9686c254ae2cf7baa753678b2fa9af20ca43
Sarkar <anindita.sarkar@capgemini.com>
2018-01-11 16:54:19 +0530

    Upgraded to 1.5.8

--- codebase/quiz-parent/quiz-ws/src/main/java/com/cyberaka/quiz/QuizBootApplication.java
+++ codebase/quiz-parent/quiz-ws/src/main/java/com/cyberaka/quiz/QuizBootApplication.java
@@ -2,30 +2,33 @@ package com.cyberaka.quiz;
 
 import java.util.logging.Logger;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 /**
  * Entry point into the quiz boot application.
  *
  * @author anindita
  */
-@SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.cyberaka.quiz.dao")
-@EntityScan(basePackages = "com.cyberaka.quiz.domain")
-@ComponentScan(basePackages = { "com.cyberaka.quiz", "com.cyberaka.quiz.rest", "com.cyberaka.quiz.dao",
-		"com.cyberaka.quiz.service.impl" })
-public class QuizBootApplication { //extends SpringBootServletInitializer {
 
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(QuizBootApplication.class);
-//	}
+@SpringBootApplication
+
+public class QuizBootApplication { // extends SpringBootServletInitializer {
+
+	// @Override
+	// protected SpringApplicationBuilder configure(SpringApplicationBuilder
+	// application) {
+	// return application.sources(QuizBootApplication.class);
+	// }
 
 	static final Logger LOG = Logger.getLogger(QuizBootApplication.class.getName());
 
@@ -40,4 +43,8 @@ public class QuizBootApplication { //extends SpringBootServletInitializer {
 	public void info() {
 		LOG.info("Quiz Boot Applicaiton");
 	}
+
+	
+
+	
 }
